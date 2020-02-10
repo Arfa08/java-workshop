165f59c1f46a526006e55eb463ca3da7b13a1362
ASUS <ASUS@DESKTOP-SBQS4FS>
2020-02-10 13:06:24 -0800

    polymorphism

---
+++ ADYPU/src/polymorphism/MethodOverRidding.java
@@ -0,0 +1,28 @@
+package polymorphism;
+
+public class MethodOverridding extends MethodOverLoading  {
+	 int a=200;
+
+	    public MethodOverriding() {
+	        super();
+	        System.out.println("Inside Child class constructor");
+	    }
+
+	    void add(int a, int b){
+	        System.out.println("Overriding Addition : "+(a+b));
+	    }
+
+	    void showData(){
+	        super.add(10,20);
+	        System.out.println(super.a);
+	    }
+
+	    public static void main(String[] args) {
+	        MethodOverriding overriding=new MethodOverriding();
+	        overriding.showData();
+	    }
+
+	
+	
+
+}
